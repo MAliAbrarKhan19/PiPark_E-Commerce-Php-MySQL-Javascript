6c1c08d0a00c2094705463bda627fae0a50caf34
M. Ali Abrar Khan <31814733+MAliAbrarKhan19@users.noreply.github.com>
2021-09-16 22:34:28 +0600

    admin

--- admin/footer.php
+++ admin/footer.php
@@ -2,11 +2,11 @@
     
     
 <!--    Footer Starts   -->
-   <div class="row justify-content-center bg-warning " style="margin-top: 4px;">
-    <div class="col-md-4">
+   <div class="row justify-content-center bg-danger " style="margin-top: 0px;">
+    <div class="col-md-6">
             
                 
-            <p class="h6 " style="color:#fcfcf9; ">2020 &copy;Copyright all rights reserved. <a href="#">ID-123</a> </p>
+            <p class="h6 " style="color:#fcfcf9; ">&copy; 2021 Copyright all rights reserved. Developed by <a href="#">ID-135</a> </p>
                 
             
         </div>
@@ -23,9 +23,9 @@
     
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     
-      <script src="../js/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="../js/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="../js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+      <script src="./js/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="./js/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
     
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
